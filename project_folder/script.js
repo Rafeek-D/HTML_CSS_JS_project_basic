b78dba410e1d7415dfe6b9a523452b4a744e83b2
rafeek-d <rafeek@test.com>
2025-05-04 01:08:08 +0200

    basic to do js

--- project_folder/script.js
+++ project_folder/script.js
@@ -1,6 +1,8 @@
 let count=0;
+let count2=2;
 
-basketlist= [];
+
+let basketlist= [];
 
 function incrementthelist_sh(){
     count++;
@@ -14,9 +16,46 @@ function incrementthelist_sh(){
 
 function incrementthelist_m(){
     count++;
+    count2 = count;
+
     basketlist.push("Mlukhiye");
     console.log("you bayed Mlukhiye");
     console.log(basketlist);
     document.getElementById('countenerid').textContent=count;
 
 }
+
+function show_bay_count(){
+   
+    //document.getElementById('countenerid').textContent=count;
+}
+
+function show_bay_count_sh(){
+    fetch('home-final-project.html');
+    //document.getElementById('countenerid').textContent=count;
+}
+
+
+function show_bay_count_mu(){
+    fetch('home-final-project.html');
+    //document.getElementById('countenerid').textContent=count;
+}
+
+
+
+class Person{
+    constructor (name,adress){
+        this.name = name;
+        this.adress= adress;
+    }
+
+
+    get_name(){
+        return this.name;
+    }
+
+    show_person(){
+        return "the person hase the name " + this.name + " and the adress: "+ this.adress;
+    }
+}
+
